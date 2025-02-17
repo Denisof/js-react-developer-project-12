2eef47a993839ce4fa917da0fae3a88ba1eae9dd
denysloshchakov <dloschakov@gmail.com>
2025-02-17 16:50:53 +0100

    viteLogo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const viteLogo = "https://vitejs.dev/logo.svg"
   return (
     <>
       <div>
